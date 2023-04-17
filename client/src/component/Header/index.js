2be59cba52f8871f7a646c59846689fbc722ebeb
nkp1111 <neerajp4321@gmail.com>
2023-04-17 16:04:53 +0530

    feat: add header content and styles

--- client/src/component/Header/index.js
+++ client/src/component/Header/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import toast from 'react-hot-toast'
+import { CgProfile } from 'react-icons/cg'
+import { MdGroup, MdArrowDropDown } from 'react-icons/md'
+import { TbLogout } from 'react-icons/tb'
 
 import { notFicStyles, images } from '../../utils'
-
+import "./header.css"
 
 const { errorStyle, successStyle } = notFicStyles
 
 const Index = ({ userData }) => {
 
-  // const { name = "something", img } = userData
-
   const logout = (e) => {
     const route = "/user/logout"
     const url = process.env.NODE_ENV !== "development" ? route : "http://localhost:5000" + route
     fetch(url, {
-      method: "POST"
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
     })
       .then(res => res.json())
       .then(data => {
@@ -46,36 +50,40 @@ const Index = ({ userData }) => {
           <a className="navbar-brand" href="/profile">
             <img src={images.logo} alt="logo" />
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
+
+          <div className="profile d-flex align-items-center ms-auto">
+            <div>
+              <img src="" alt="" width="50" height="50" />
+            </div>
+            {"Dummy name"}
           </div>
 
-          <button className='btn ms-auto'
-            onClick={(e) => logout(e)}
-          >
-            Logout
-          </button>
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <button className="dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                {/* <MdArrowDropDown /> */}
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a className="dropdown-item" href="/profile">
+                    <CgProfile /> My Profile
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#"><MdGroup />Group Chat</a>
+                </li>
+
+                <li><hr className="dropdown-divider" /></li>
+
+                <li>
+                  <a className="dropdown-item logout-item" href="#"
+                    onClick={(e) => logout(e)}>
+                    <TbLogout />Log Out
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
