ef8ae1f94d958cdd547b06521a85f3f232d52437
nkp1111 <neerajp4321@gmail.com>
2023-04-18 17:45:46 +0530

    feat: update user info and add image to cloudinary

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const { cloudinary } = require("../cloudinary")
+const User = require("../db/userSchema")
 
 router.post("/edit", (req, res) => {
   const { image, name, email, password, bio, phone, id } = req.body
@@ -11,12 +12,28 @@ router.post("/edit", (req, res) => {
     .upload(image, {
       folder: "Auth-app"
     })
-    .then(res => {
-      console.log(res)
-      const imageUrl = { url: res.url, secure: res.secure_url }
-    })
+    .then(async res => {
 
-  res.send({ "success": "Edited info" })
+      const imageUrl = { url: res.url, secure: res.secure_url }
+      const user = await User.findById(id)
+      user.name = name
+      user.image = imageUrl
+      user.phone = phone
+      user.bio = bio
+      user.username = email
+      try {
+        const newUser = await User.register(user, password)
+        res.cookie("sessionId", req.session.id)
+        res.send({ success: "User updated", user: newUser })
+      } catch (error) {
+        res.send({ error: "Error Updating profile" })
+      }
+    })
 })
 
+
+router.get("/show", (req, res) => {
+  console.log(req.user, req.cookies)
+  res.send({ error: "Error while getting Info" })
+})
 module.exports = router
